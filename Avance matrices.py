6b9c178f6e2a0e8c85b1909f154fc8f3ce9a5a1b
jeblanca <jesuse.blancam2005@gmail.com>
2025-06-15 08:45:50 -0400

    Update Avance matrices.py

--- Avance matrices.py
+++ Avance matrices.py
@@ -6,7 +6,7 @@ from numpy import *
 from random import *
 def imprimir_pos(matrizx):
     posiciones = [] #Probé con los de Numpy pero me funcionó mejor esto
-    for i in range(len(matrizx),1):
+    for i in range(len(matrizx)):
         for j in range(len(matrizx[0])):
             if matrizx[i][j] == 1:
                 posiciones.append((i, j))
@@ -15,7 +15,7 @@ def imprimir_pos(matrizx):
 
 
 def generar_matrices_user(filas,columnas):
-    matriz = []
+    matriz = [] #La matriz tiene que estar vacía de esta forma)
     for _ in range(0,filas,1):
         fila = []
         for i in range(0,columnas,1):
@@ -26,32 +26,26 @@ def generar_matrices_user(filas,columnas):
     return matriz
 
 def contar_vecinos(matriz,x,y):
-    """
-    Cuenta cauntas celulas vivas ( valor de 1) hay alrededor de la celda (x,y)
-    (fila,columna). considerando los bordes como celulas vacias.
-    """
+    # Cuenta cuantas celulas vivas (Representadas con el número uno) hay alrededor de la celda (x,y), (fila,columna). considerando los bordes como celulas vacias.
     vecinos = 0
     filas,columnas = matriz.shape
     # Desplazamiento en filas y columnas
     desplazamiento = [-1, 0, 1] 
-    # recorrer las posiciones vecinas (inclyendo diagonales)
+    # Recorrer las posiciones vecinas (inclyendo diagonales)
     for dx in desplazamiento: # Desplazamiento en filas
         for dy in desplazamiento: # Desplazamiento en columnas
             if dx == 0 and dy == 0:
-                continue # saltar la celda central (x,y)
+                continue # Saltar la celda central (x,y)
             else:
-                nx = x + dx # fila vecina
-                ny = y + dy # columna vecina
-                # verificar si la posicion esta dentro de los limites
+                nx = x + dx # Fila vecina
+                ny = y + dy # Columna vecina
+                # Verificar si la posicion esta dentro de los limites
                 if 0 <= nx < filas and 0 <= ny < columnas:
                     if matriz[nx][ny] == 1:
                         vecinos += 1
     return vecinos 
 
-def siguiente_generacion(matriz):
-    """
-    Aplica las reglas y devuelve la nueva matriz
-    """
+def siguiente_generacion(matriz): #Aplica las reglas y devuelve la nueva matriz
     # Asegurarse que sea un arreglo de NUMPY
     matriz=np.array(matriz)
     filas,columnas = matriz.shape
@@ -79,9 +73,9 @@ def siguiente_generacion(matriz):
 
 
 def main():
-    filas = int(input('Ingrese el numero de filas : '))
+    filas = int(input('Ingrese el numero de filas : ')) #Aún no le puesto límite
     columnas = int(input("Ingrese el numero de columnas:"))
-    m1 = generar_matrices_user(filas,columnas)
+    m1 = generar_matrices_user(filas,columnas) #Matriz principal
     m2 = siguiente_generacion(matriz=m1)
     print("Matriz inicial")
     print(m1)
